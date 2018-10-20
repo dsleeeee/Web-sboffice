2fd5795353f6eb16a0fc338bafda1a10e4f4e587
Hyunsoo <hsnoh@solbipos.co.kr>
2018-10-20 14:45:39 +0900

    상품정보관리 작업중

--- src/main/java/kr/co/solbipos/base/prod/prod/service/impl/ProdMapper.java
+++ src/main/java/kr/co/solbipos/base/prod/prod/service/impl/ProdMapper.java
@@ -14,7 +14,8 @@ import java.util.List;
  * @
  * @  수정일      수정자              수정내용
  * @ ----------  ---------   -------------------------------
- * @ 2018.08.06  장혁수      최초생성
+ * @ 2018.08.06  장혁수       최초생성
+ * @ 2018.10.19  노현수       상품조회 관련 변경
  *
  * @author NHN한국사이버결제 KCP 장혁수
  * @since 2018. 08.06
@@ -27,44 +28,24 @@ import java.util.List;
 public interface ProdMapper {
 
     /**
-     * 매장 상품 조회
+     * 상품 조회
      * @param prodVO
      * @return List
      */
     List<DefaultMap<String>> getProdList(ProdVO prodVO);
 
     /**
-     * 매장 상품 상세 조회
+     * 상품 상세 조회
      * @param prodVO
      * @return DefaultMap
      */
     DefaultMap<String> getProdDetail(ProdVO prodVO);
 
     /**
-     * 매장 연결상품 조회
+     * 연결상품 조회
      * @param prodVO
      * @return List
      */
     List<DefaultMap<String>> getUnitstProdList(ProdVO prodVO);
 
-    /**
-     * 본사 상품 조회
-     * @param prodVO
-     * @return List
-     */
-    List<DefaultMap<String>> getHqProdList(ProdVO prodVO);
-
-    /**
-     * 본사 상품 상세 조회
-     * @param prodVO
-     * @return DefaultMap
-     */
-    DefaultMap<String> getHqProdDetail(ProdVO prodVO);
-
-    /**
-     * 본사 연결상품 조회
-     * @param prodVO
-     * @return List
-     */
-    List<DefaultMap<String>> getHqUnitstProdList(ProdVO prodVO);
 }
