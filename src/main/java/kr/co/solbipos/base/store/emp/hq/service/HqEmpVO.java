bd6bbbe2e2e889ea474dd8e4206d3fc3f25a2449
jekim <jekim@solbipos.co.kr>
2018-11-19 21:35:17 +0900

    1. 임시로 오류 수정. 추후 angular로 변경 필요

--- src/main/java/kr/co/solbipos/base/store/emp/hq/service/HqEmpVO.java
+++ src/main/java/kr/co/solbipos/base/store/emp/hq/service/HqEmpVO.java
@@ -100,6 +100,8 @@ public class HqEmpVO extends PageVO {
     /** 권한 그룹 코드 */
     String authGrpCd;
 
+    private boolean chkDt;
+
     public String getHqOfficeCd() {
         return hqOfficeCd;
     }
@@ -276,4 +278,18 @@ public class HqEmpVO extends PageVO {
         this.authGrpCd = authGrpCd;
     }
 
+    /**
+     * @return the chkDt
+     */
+
+    public boolean isChkDt() {
+        return chkDt;
+    }
+
+    /**
+     * @param chkDt the chkDt to set
+     */
+    public void setChkDt(boolean chkDt) {
+        this.chkDt = chkDt;
+    }
 }
