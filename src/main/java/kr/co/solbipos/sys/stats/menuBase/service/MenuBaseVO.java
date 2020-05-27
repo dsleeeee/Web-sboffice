b9b659405fc7b59f3fac02c294e0b231d57ebea7
sdffd2605 <sdffd2605@solbipos.co.kr>
2020-05-27 13:56:08 +0900

    시스템관리 > 통계 > 메뉴기준 사용현황 - 메뉴기준 사용현황 화면개발

--- src/main/java/kr/co/solbipos/sys/stats/menuBase/service/MenuBaseVO.java
+++ src/main/java/kr/co/solbipos/sys/stats/menuBase/service/MenuBaseVO.java
@@ -50,12 +50,6 @@ public class MenuBaseVO extends PageVO {
 
     /** 대,중,소메뉴 */
     private String level;
-//    /** 대메뉴 */
-//    private String level1;
-//    /** 중메뉴 */
-//    private String level2;
-//    /** 소메뉴 */
-//    private String level3;
 
     public String getResrceNm() { return resrceNm; }
 
@@ -120,28 +114,4 @@ public class MenuBaseVO extends PageVO {
     public void setLevel(String level) {
         this.level = level;
     }
-
-//    public String getLevel1() {
-//        return level1;
-//    }
-//
-//    public void setLevel1(String level1) {
-//        this.level1 = level1;
-//    }
-//
-//    public String getLevel2() {
-//        return level2;
-//    }
-//
-//    public void setLevel2(String level2) {
-//        this.level2 = level2;
-//    }
-//
-//    public String getLevel3() {
-//        return level3;
-//    }
-//
-//    public void setLevel3(String level3) {
-//        this.level3 = level3;
-//    }
 }
