e479fc74521693654ccbc19ffc5906d3f087ba0a
dslee <dslee@solbipos.co.kr>
2020-08-14 16:24:30 +0900

    1. [기초관리] - [상품관리] - [상품이미지등록] : 화면개발 중

---
+++ src/main/java/kr/co/solbipos/base/prod/prodImg/service/ProdImgService.java
@@ -0,0 +1,32 @@
+package kr.co.solbipos.base.prod.prodImg.service;
+
+import kr.co.common.data.structure.DefaultMap;
+import kr.co.solbipos.application.session.auth.service.SessionInfoVO;
+import kr.co.solbipos.base.prod.prod.service.ProdVO;
+
+import java.util.List;
+
+/**
+ * @Class Name : ProdImgService.java
+ * @Description : 기초관리 - 상품관리 - 상품이미지관리
+ * @Modification Information
+ * @
+ * @  수정일      수정자              수정내용
+ * @ ----------  ---------   -------------------------------
+ * @ 2020.08.13  이다솜       최초생성
+ *
+ * @author 솔비포스 개발본부 백엔드PT 이다솜
+ * @since 2020. 08.13
+ * @version 1.0
+ *
+ *  Copyright (C) by SOLBIPOS CORP. All right reserved.
+ */
+
+public interface ProdImgService {
+
+    /** 상품이미지관리 - 상품목록조회 */
+    List<DefaultMap<String>> getProdList(ProdImgVO prodImgVO, SessionInfoVO sessionInfoVO);
+
+    /** 상품이미지관리 - 상품이미지조회 */
+    List<DefaultMap<String>> getProdImg(ProdImgVO prodImgVO, SessionInfoVO sessionInfoVO);
+}
