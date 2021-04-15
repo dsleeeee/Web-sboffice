f08214dc1e3aa55d5f3a46903dbf1fcb374fa872
jskim <1117kjs@naver.com>
2021-04-15 11:28:29 +0900

    SBPOS-582 [솔비포스][W] 상반기 모의해킹 결과(보안취약점) 조치(파일업로드/다운로드) - 적용

--- src/main/java/kr/co/solbipos/adi/board/board/web/BoardController.java
+++ src/main/java/kr/co/solbipos/adi/board/board/web/BoardController.java
@@ -291,11 +291,12 @@ public class BoardController {
     public void getBoardDetailAtchDownload(BoardVO boardVO, HttpServletRequest request,
                                        HttpServletResponse response, Model model) throws Exception {
 
-System.out.println("kjs: request.getParameter(fileNm): "  + request.getParameter("fileNm"));
-System.out.println("kjs: boardVO.getFileNm() : "  + boardVO.getFileNm());
+//System.out.println("kjs: request.getParameter(fileNm): "  + request.getParameter("fileNm"));
+//System.out.println("kjs: boardVO.getFileNm() : "  + boardVO.getFileNm());
 
 //        File file = new File("D:\\Workspace\\javaWeb\\testBoardAtch\\", boardVO.getFileNm());
-        String reFileNM = boardVO.getFileNm().replaceAll("..", "").replaceAll("/", "");
+        String reFileNM = boardVO.getFileNm().replaceAll("../", "");
+
 System.out.println("kjs: reFileNM : " + reFileNM);
         File file = new File(BaseEnv.FILE_UPLOAD_DIR + "board/", reFileNM);
 
