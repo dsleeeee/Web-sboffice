2e2704709fad66dfc3639cdbdef5978880430955
jih-k <jhkwon@solbipos.co.kr>
2021-04-20 11:30:29 +0900

    1. 가격확인(정수9자리) isInteger

--- src/main/webapp/resources/solbipos/js/base/price/salePrice/storeSalePrice.js
+++ src/main/webapp/resources/solbipos/js/base/price/salePrice/storeSalePrice.js
@@ -248,6 +248,13 @@ app.controller('storeSalePriceCtrl', ['$scope', '$http', function ($scope, $http
       return false;
     }
 
+    // isInteger는 es6 임. ie 11 에서는 안되므로 함수 만듬.
+    Number.isInteger = Number.isInteger || function(value) {
+      return typeof value === "number" &&
+          isFinite(value) &&
+          Math.floor(value) === value;
+    };
+
     // 파라미터 설정
     var params = new Array();
     var saleAmtOption = $scope.prodInfo.saleAmtOption;
