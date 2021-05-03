2ffc05554f1a5a8a4bddd1a2464817ad730f7fca
sdffd2605 <sdffd2605@solbipos.co.kr>
2021-05-03 15:51:15 +0900

    주석 수정

--- src/main/java/kr/co/solbipos/mobile/sale/today/todaySale/web/TodaySaleController.java
+++ src/main/java/kr/co/solbipos/mobile/sale/today/todaySale/web/TodaySaleController.java
@@ -24,6 +24,21 @@ import java.util.List;
 
 import static kr.co.common.utils.grid.ReturnUtil.returnJson;
 
+/**
+ * @Class Name : TodaySaleController.java
+ * @Description : (모바일) 매출현황 > 당일매출현황
+ * @Modification Information
+ * @
+ * @  수정일      수정자              수정내용
+ * @ ----------  ---------   -------------------------------
+ * @ 2021.04.02  김설아      최초생성
+ *
+ * @author 솔비포스 개발본부 백엔드PT 김설아
+ * @since 2021.04.02
+ * @version 1.0
+ *
+ *  Copyright (C) by SOLBIPOS CORP. All right reserved.
+ */
 @Controller
 @RequestMapping("/mobile/sale/today/todaySale")
 public class TodaySaleController {
