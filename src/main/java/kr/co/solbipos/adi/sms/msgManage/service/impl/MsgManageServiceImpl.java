3792cf7640a99f6d15b5062a778f3e9363b71b53
sdffd2605 <sdffd2605@solbipos.co.kr>
2021-06-28 10:46:13 +0900

    부가서비스>SMS관리>메세지관리 - 메세지관리 화면개발

---
+++ src/main/java/kr/co/solbipos/adi/sms/msgManage/service/impl/MsgManageServiceImpl.java
@@ -0,0 +1,132 @@
+package kr.co.solbipos.adi.sms.msgManage.service.impl;
+
+import kr.co.common.data.structure.DefaultMap;
+import kr.co.common.utils.spring.StringUtil;
+import kr.co.common.utils.jsp.CmmEnvUtil;
+import kr.co.solbipos.application.session.auth.service.SessionInfoVO;
+import kr.co.solbipos.adi.sms.msgManage.service.MsgManageService;
+import kr.co.solbipos.adi.sms.msgManage.service.MsgManageVO;
+import kr.co.solbipos.application.session.user.enums.OrgnFg;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import kr.co.solbipos.application.com.griditem.enums.GridDataFg;
+
+import java.util.List;
+
+import static kr.co.common.utils.DateUtil.currentDateTimeString;
+
+/**
+ * @Class Name : MsgManageServiceImpl.java
+ * @Description : 부가서비스 > SMS관리 > 메세지관리
+ * @Modification Information
+ * @
+ * @  수정일      수정자              수정내용
+ * @ ----------  ---------   -------------------------------
+ * @ 2021.06.22  김설아      최초생성
+ *
+ * @author 솔비포스 개발본부 WEB개발팀 김설아
+ * @since 2021.06.22
+ * @version 1.0
+ *
+ *  Copyright (C) by SOLBIPOS CORP. All right reserved.
+ */
+@Service("msgManageService")
+@Transactional
+public class MsgManageServiceImpl implements MsgManageService {
+    private final MsgManageMapper msgManageMapper;
+
+    /**
+     * Constructor Injection
+     */
+    @Autowired
+    public MsgManageServiceImpl(MsgManageMapper msgManageMapper) {
+        this.msgManageMapper = msgManageMapper;
+    }
+
+    /** 메세지관리 - 그룹 조회 */
+    @Override
+    public List<DefaultMap<Object>> getMsgManageList(MsgManageVO msgManageVO, SessionInfoVO sessionInfoVO) {
+
+        msgManageVO.setOrgnCd(sessionInfoVO.getOrgnCd());
+
+        return msgManageMapper.getMsgManageList(msgManageVO);
+    }
+
+    /** 메세지관리 - 그룹 저장 */
+    @Override
+    public int getMsgManageSave(MsgManageVO[] msgManageVOs, SessionInfoVO sessionInfoVO) {
+
+        int procCnt = 0;
+        String currentDt = currentDateTimeString();
+
+        for(MsgManageVO msgManageVO : msgManageVOs) {
+
+            msgManageVO.setOrgnCd(sessionInfoVO.getOrgnCd());
+            msgManageVO.setModDt(currentDt);
+            msgManageVO.setModId(sessionInfoVO.getUserId());
+
+            if (msgManageVO.getStatus() == GridDataFg.INSERT) {
+                msgManageVO.setRegDt(currentDt);
+                msgManageVO.setRegId(sessionInfoVO.getUserId());
+
+                // 그룹코드(자동 채번)
+                String msgGrpCd = msgManageMapper.getMsgManageMsgGrpCd(msgManageVO);
+                msgManageVO.setMsgGrpCd(msgGrpCd);
+
+                procCnt = msgManageMapper.getMsgManageSaveInsert(msgManageVO);
+
+            } else if(msgManageVO.getStatus() == GridDataFg.UPDATE) {
+                procCnt = msgManageMapper.getMsgManageSaveUpdate(msgManageVO);
+
+            } else if (msgManageVO.getStatus() == GridDataFg.DELETE) {
+                procCnt = msgManageMapper.getMsgManageSaveDelete(msgManageVO);
+
+                // 메세지서식 전체 삭제
+                procCnt = msgManageMapper.getMsgManageDtlSaveDeleteAll(msgManageVO);
+            }
+        }
+
+        return procCnt;
+    }
+
+    /** 메세지관리 - 메세지서식 조회 */
+    @Override
+    public List<DefaultMap<Object>> getMsgManageDtlList(MsgManageVO msgManageVO, SessionInfoVO sessionInfoVO) {
+
+        msgManageVO.setOrgnCd(sessionInfoVO.getOrgnCd());
+
+        return msgManageMapper.getMsgManageDtlList(msgManageVO);
+    }
+
+    /** 메세지관리 - 메세지서식 저장 */
+    @Override
+    public int getMsgManageDtlSave(MsgManageVO msgManageVO, SessionInfoVO sessionInfoVO) {
+
+        int procCnt = 0;
+        String currentDt = currentDateTimeString();
+
+        msgManageVO.setOrgnCd(sessionInfoVO.getOrgnCd());
+        msgManageVO.setModDt(currentDt);
+        msgManageVO.setModId(sessionInfoVO.getUserId());
+
+        if (msgManageVO.getStatus() == GridDataFg.INSERT) {
+            msgManageVO.setRegDt(currentDt);
+            msgManageVO.setRegId(sessionInfoVO.getUserId());
+
+            // SEQ_NO(자동 채번)
+            String seqNo = msgManageMapper.getMsgManageMsgSeqNo(msgManageVO);
+            msgManageVO.setSeqNo(seqNo);
+
+            procCnt = msgManageMapper.getMsgManageDtlSaveInsert(msgManageVO);
+
+        } else if(msgManageVO.getStatus() == GridDataFg.UPDATE) {
+            procCnt = msgManageMapper.getMsgManageDtlSaveUpdate(msgManageVO);
+
+        } else if (msgManageVO.getStatus() == GridDataFg.DELETE) {
+            procCnt = msgManageMapper.getMsgManageDtlSaveDelete(msgManageVO);
+        }
+
+        return procCnt;
+    }
+}
---
+++ src/main/webapp/resources/solbipos/js/adi/sms/msgManage/msgManage.js
@@ -0,0 +1,333 @@
+/****************************************************************
+ *
+ * 파일명 : msgManage.js
+ * 설  명 : 메세지관리 JavaScript
+ *
+ *    수정일      수정자      Version        Function 명
+ * ------------  ---------   -------------  --------------------
+ * 2021.06.22     김설아      1.0
+ *
+ * **************************************************************/
+/**
+ * get application
+ */
+var app = agrid.getApp();
+
+// 메세지관리 목록 수정
+function msgModify(seqNo, title, message) {
+    var scope = agrid.getScope('msgManageDtlCtrl');
+    var params = {};
+    params.seqNo = seqNo;
+    params.title = title;
+    params.message = message;
+    scope.msgModify(params);
+}
+
+// 메세지관리 목록 삭제
+function msgDel(seqNo) {
+    var scope = agrid.getScope('msgManageDtlCtrl');
+    var params = {};
+    params.seqNo = seqNo;
+    scope.msgDel(params);
+}
+
+/**
+ *  메세지관리 조회 그리드 생성
+ */
+app.controller('msgManageCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    // 상위 객체 상속 : T/F 는 picker
+    angular.extend(this, new RootController('msgManageCtrl', $scope, $http, false));
+
+    // grid 초기화 : 생성되기전 초기화되면서 생성된다
+    $scope.initGrid = function (s, e) {
+        // 그리드 링크 효과
+        s.formatItem.addHandler(function (s, e) {
+            if (e.panel === s.cells) {
+                var col = s.columns[e.col];
+
+                // 그룹코드
+                if (col.binding === "msgGrpCd") {
+                    // var item = s.rows[e.row].dataItem;
+                    wijmo.addClass(e.cell, 'wijLink');
+                }
+            }
+        });
+
+        // 그리드 선택 이벤트
+        s.addEventListener(s.hostElement, 'mousedown', function(e) {
+            var ht = s.hitTest(e);
+            if( ht.cellType === wijmo.grid.CellType.Cell) {
+                var col = ht.panel.columns[ht.col];
+
+                // 그룹코드 클릭시 상세정보 조회
+                if ( col.binding === "msgGrpCd") {
+                    var selectedRow = s.rows[ht.row].dataItem;
+                    var params = {};
+                    params.msgGrpCd = selectedRow.msgGrpCd;
+                    params.msgGrpNm = selectedRow.msgGrpNm;
+
+                    var storeScope = agrid.getScope('msgManageDtlCtrl');
+                    storeScope._broadcast('msgManageDtlCtrl', params);
+                    event.preventDefault();
+                }
+            }
+        });
+
+        // 조회
+        $scope.searchMsgManage();
+    };
+
+    // <-- 검색 호출 -->
+    $scope.$on("msgManageCtrl", function(event, data) {
+        $scope.searchMsgManage();
+        event.preventDefault();
+    });
+
+    $scope.searchMsgManage = function() {
+        var params = {};
+
+        $scope._inquiryMain("/adi/sms/msgManage/msgManage/getMsgManageList.sb", params, function() {
+            $scope.$apply(function() {
+                var storeScope = agrid.getScope('msgManageDtlCtrl');
+                storeScope._broadcast('msgManageDtlCtrl', null);
+            });
+        }, false);
+    };
+
+    $scope.searchMsgManageAllSearch = function(data) {
+        var params = {};
+
+        $scope._inquiryMain("/adi/sms/msgManage/msgManage/getMsgManageList.sb", params, function() {
+            $scope.$apply(function() {
+                var storeScope = agrid.getScope('msgManageDtlCtrl');
+                storeScope._broadcast('msgManageDtlCtrl', data);
+            });
+        }, false);
+    };
+    // <-- //검색 호출 -->
+
+    // <-- 그리드 행 추가 -->
+    $scope.addRow = function() {
+        // 파라미터 설정
+        var params = {};
+        params.status = "I";
+        params.gChk = true;
+        params.msgGrpCd="자동채번";
+        params.msgGrpNm = "";
+        params.msgGrpCnt = "0";
+
+        // 추가기능 수행 : 파라미터
+        $scope._addRow(params);
+    };
+    // <-- //그리드 행 추가 -->
+
+    // <-- 그리드 행 삭제 -->
+    $scope.del = function(){
+        // 메세지그룹을 삭제하시면 <br/> 해당 그룹의 메세지 전체가 삭제됩니다. <br/> 그래도 삭제하시겠습니까?
+        $scope._popConfirm(messages["msgManage.delMsgConfirm"], function() {
+            for(var i = $scope.flex.collectionView.items.length-1; i >= 0; i-- ){
+                var item = $scope.flex.collectionView.items[i];
+
+                if(item.gChk) {
+                    $scope.flex.collectionView.removeAt(i);
+                }
+            }
+
+            // 저장
+            $scope.save();
+        });
+    };
+    // <-- //그리드 행 삭제 -->
+
+    // <-- 그리드 저장 -->
+    $scope.save = function() {
+        for (var i = 0; i < $scope.flex.collectionView.items.length; i++) {
+            if($scope.flex.collectionView.items[i].msgGrpNm === "") {
+                $scope._popMsg(messages["msgManage.msgGrpNmBlank"]); // 그룹명을 입력해주세요.
+                return false;
+            }
+        }
+
+        // 파라미터 설정
+        var params = new Array();
+        for (var i = 0; i < $scope.flex.collectionView.itemsEdited.length; i++) {
+            $scope.flex.collectionView.itemsEdited[i].status = "U";
+            params.push($scope.flex.collectionView.itemsEdited[i]);
+        }
+        for (var i = 0; i < $scope.flex.collectionView.itemsAdded.length; i++) {
+            $scope.flex.collectionView.itemsAdded[i].status = "I";
+            params.push($scope.flex.collectionView.itemsAdded[i]);
+        }
+        for (var i = 0; i < $scope.flex.collectionView.itemsRemoved.length; i++) {
+            $scope.flex.collectionView.itemsRemoved[i].status = "D";
+            params.push($scope.flex.collectionView.itemsRemoved[i]);
+        }
+
+        // 저장기능 수행 : 저장URL, 파라미터, 콜백함수
+        $scope._postJSONSave.withPopUp("/adi/sms/msgManage/msgManage/getMsgManageSave.sb", params, function(){ $scope.searchMsgManage();});
+    };
+    // <-- //그리드 저장 -->
+}]);
+
+
+/**
+ *  메세지관리 상세 조회 그리드 생성
+ */
+app.controller('msgManageDtlCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    // 상위 객체 상속 : T/F 는 picker
+    angular.extend(this, new RootController('msgManageDtlCtrl', $scope, $http, false));
+
+    // grid 초기화 : 생성되기전 초기화되면서 생성된다
+    $scope.initGrid = function (s, e) {
+    };
+
+    $("#lblTxtByte").text("0");
+
+    // <-- 검색 호출 -->
+    $scope.$on("msgManageDtlCtrl", function(event, data) {
+        $("#divMsgComment").html("");
+        // 신규
+        $scope.msgNew();
+
+        if(data != undefined) {
+            $("#lblMsgGrpCd").text(data.msgGrpCd);
+            $("#lblMsgGrpNmInfo").text(" / ");
+            $("#lblMsgGrpNm").text(data.msgGrpNm);
+
+            $scope.searchMsgManageDtl(data);
+
+        } else {
+            $("#lblMsgGrpCd").text("");
+            $("#lblMsgGrpNmInfo").text("");
+            $("#lblMsgGrpNm").text("");
+        }
+        event.preventDefault();
+    });
+
+    $scope.searchMsgManageDtl = function(data) {
+        var params = {};
+        params.msgGrpCd = data.msgGrpCd;
+
+        $scope._postJSONQuery.withOutPopUp("/adi/sms/msgManage/msgManage/getMsgManageDtlList.sb", params, function(response) {
+            var list = response.data.data.list;
+            var innerHtml = "";
+
+            if(list.length > 0) {
+                for(var i=0; i < list.length; i++) {
+                    innerHtml += "<div style=\"float:left; text-align:center; width:125px; height:160px; padding-top:10px; padding-right:10px;\">";
+                    innerHtml += "<table>";
+                    innerHtml += "<colgroup>";
+                    innerHtml += "<col class=\"w100\" />";
+                    innerHtml += "</colgroup>";
+                    innerHtml += "<tbody>";
+                    innerHtml += "<table>";
+                    innerHtml += "<tr><td><input type=\"text\" class=\"sb-input-msg w100\" value=\""+ list[i].title +"\" readonly/></td></tr>";
+                    innerHtml += "<tr style=\"height: 10px\"></tr>";
+                    innerHtml += "<tr><td><textarea style=\"width:100%; height:90px; overflow-x:hidden; background-color: #EAF7FF\" readonly>" + list[i].message + "</textarea></td></tr>";
+                    innerHtml += "<tr style=\"height: 10px\"></tr>";
+                    innerHtml += "<tr><td>";
+                    innerHtml += "<span style=\"padding-right: 10px;\"><a href=\"#\" onclick=\"msgModify(\'"+ list[i].seqNo + "\', \'"+ list[i].title + "\', \'"+ list[i].message + "\')\"><img src=\"/resource/solbipos/css/img/sms/btn_upd.jpg\"></a></span>";
+                    innerHtml += "<span><a href=\"#\" onclick=\"msgDel(\'"+ list[i].seqNo + "\')\"><img src=\"/resource/solbipos/css/img/sms/btn_del.jpg\"></a></span>";
+                    innerHtml += "</td></tr>";
+                    innerHtml += "</table>";
+                    innerHtml += "</div>";
+                }
+                $("#divMsgComment").html(innerHtml);
+            }
+        }, false);
+    };
+    // <-- //검색 호출 -->
+
+    // 자동변환
+    $scope.addMsg = function(str) {
+        var messageContent = document.getElementById("messageContent");
+        messageContent.focus();
+        messageContent.innerHTML = messageContent.innerHTML + str;
+
+        // 바이트
+        $scope.showByte();
+    };
+
+    // 바이트
+    $scope.showByte = function() {
+        var messageContent = document.getElementById("messageContent");
+        $("#lblTxtByte").text(messageContent.value.getByteLength());
+    };
+
+    // 신규
+    $scope.msgNew = function () {
+        $("#srchTitle").val("");
+        $scope.messageContent = "";
+        $("#lblTxtByte").text("0");
+        $("#lblSeqNo").text("");
+    };
+
+    // 저장
+    $scope.msgSave = function () {
+        // 바이트
+        var byte = $("#lblTxtByte").text();
+        if(byte > 80) {
+            $scope._popMsg(messages["msgManage.byteAlert"]); // 80Byte 이상은 저장할 수 없습니다.
+            return false;
+        }
+
+        var seqNo = $("#lblSeqNo").text();
+
+        $scope._popConfirm(messages["cmm.choo.save"], function() {
+            var params = {};
+            params.msgGrpCd = $("#lblMsgGrpCd").text();
+            params.title = $scope.title;
+            params.content = $scope.messageContent;
+            if(seqNo == "") {
+                params.seqNo = "";
+                params.status = "I";
+            } else {
+                params.seqNo = seqNo;
+                params.status = "U";
+            }
+
+            // 저장기능 수행 : 저장URL, 파라미터, 콜백함수
+            $scope._postJSONSave.withPopUp("/adi/sms/msgManage/msgManage/getMsgManageDtlSave.sb", params, function(){ $scope.allSearch(); });
+        });
+    };
+
+    // 재조회
+    $scope.allSearch = function () {
+        // 메세지관리 조회
+        var params = {};
+        params.msgGrpCd = $("#lblMsgGrpCd").text();
+        params.msgGrpNm = $("#lblMsgGrpNm").text();
+
+        // 신규,삭제,수정된 내역 재조회
+        var msgManageScope = agrid.getScope('msgManageCtrl');
+        msgManageScope.searchMsgManageAllSearch(params);
+    };
+
+    // 메세지관리 목록 수정
+    $scope.msgModify = function (data) {
+        $("#lblSeqNo").text(data.seqNo);
+        $("#srchTitle").val(data.title);
+
+        var messageContent = document.getElementById("messageContent");
+        messageContent.focus();
+        messageContent.innerHTML = data.message;
+
+        // 바이트
+        $scope.showByte();
+    };
+
+    // 메세지관리 목록 삭제
+    $scope.msgDel = function (data) {
+        $scope._popConfirm(messages["cmm.choo.delete"], function() {
+            var params = {};
+            params.msgGrpCd = $("#lblMsgGrpCd").text();
+            params.seqNo = data.seqNo;
+            params.status = "D";
+
+            // 저장기능 수행 : 저장URL, 파라미터, 콜백함수
+            $scope._postJSONSave.withPopUp("/adi/sms/msgManage/msgManage/getMsgManageDtlSave.sb", params, function(){ $scope.allSearch(); });
+        });
+    };
+}]);
