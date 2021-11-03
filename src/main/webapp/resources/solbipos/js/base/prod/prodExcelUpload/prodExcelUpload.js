81d4fb9030ff12bb8c83f5e4ab02f07026d55c8a
dslee <dslee@solbipos.co.kr>
2021-11-03 11:48:47 +0900

    BBQ 신규 POS 프로젝트  - [기초관리] - [상품관리] - [상품엑셀업로드]   1. 상품코드 수동채번시 숫자/영문만 입력 체크 제거

--- src/main/webapp/resources/solbipos/js/base/prod/prodExcelUpload/prodExcelUpload.js
+++ src/main/webapp/resources/solbipos/js/base/prod/prodExcelUpload/prodExcelUpload.js
@@ -444,8 +444,8 @@ app.controller('prodExcelUploadProdCtrl', ['$scope', '$http', '$timeout', functi
                     result = messages["prodExcelUpload.prodCdBlank"]; // 상품코드를 입력하세요.
                 } else {
                     // 숫자/영문만 입력
-                    var numChkexp = /[^A-Za-z0-9]/g;
-                    if (numChkexp.test($scope.flex.collectionView.items[i].prodCd)) { result = messages["prodExcelUpload.prodCdInChk"]; } // 상품코드 숫자/영문만 입력해주세요.
+                    //var numChkexp = /[^A-Za-z0-9]/g;
+                    //if (numChkexp.test($scope.flex.collectionView.items[i].prodCd)) { result = messages["prodExcelUpload.prodCdInChk"]; } // 상품코드 숫자/영문만 입력해주세요.
 
                     // 최대길이 체크
                     if (nvl($scope.flex.collectionView.items[i].prodCd, '').getByteLengthForOracle() > 13) { result = messages["prodExcelUpload.prodCdLengthChk"]; } // 상품코드 길이가 너무 깁니다.
