5cd4af80cc7feafbc3a03a12e5d1033d9e5c3a35
sdffd2605 <sdffd2605@solbipos.co.kr>
2021-11-08 16:00:49 +0900

    - 최근이력 클릭시 에러 -> 메시지 엔터 들어가게 수정해서 생긴 문제

--- src/main/webapp/resources/solbipos/js/adi/sms/marketingSmsSend/marketingSmsSend.js
+++ src/main/webapp/resources/solbipos/js/adi/sms/marketingSmsSend/marketingSmsSend.js
@@ -766,7 +766,7 @@ app.controller('marketingSmsSendCtrl', ['$scope', '$http', '$timeout', function 
                     innerHtml += "<table>";
                     innerHtml += "<tr><td><input type=\"text\" class=\"sb-input-msg w100\" value=\""+ list[i].title +"\" readonly/></td></tr>";
                     innerHtml += "<tr style=\"height: 10px\"></tr>";
-                    innerHtml += "<tr><td><textarea style=\"width:100%; height:90px; overflow-x:hidden; background-color: #EAF7FF\" onclick=\"marketingSmsSendMsgShow(\'"+ list[i].title + "\', \'"+ list[i].message + "\')\" readonly>" + list[i].message + "</textarea></td></tr>";
+                    innerHtml += "<tr><td><textarea style=\"width:100%; height:90px; overflow-x:hidden; background-color: #EAF7FF\" onclick=\"marketingSmsSendMsgShow(\'"+ list[i].title + "\', \'"+ list[i].message.replaceAll("\n","") + "\')\" readonly>" + list[i].message + "</textarea></td></tr>";
                     innerHtml += "</table>";
                     innerHtml += "</div>";
                 }
