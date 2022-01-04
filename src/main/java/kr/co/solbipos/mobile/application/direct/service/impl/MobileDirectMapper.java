1396041d6a216dc824da65ac8001e069b1cb7437
sdffd2605 <sdffd2605@solbipos.co.kr>
2022-01-04 14:36:29 +0900

    주석 수정

--- src/main/java/kr/co/solbipos/mobile/application/direct/service/impl/MobileDirectMapper.java
+++ src/main/java/kr/co/solbipos/mobile/application/direct/service/impl/MobileDirectMapper.java
@@ -9,16 +9,16 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 /**
- * @Class Name : MobileDaySaleMapper.java
+ * @Class Name : MobileDirectMapper.java
  * @Description : (모바일) QR > 원하는 페이지 오픈
  * @Modification Information
  * @
  * @  수정일      수정자              수정내용
  * @ ----------  ---------   -------------------------------
- * @ 2021.05.04  김설아      최초생성
+ * @ 2021.11.26  권지현      최초생성
  *
- * @author 솔비포스 개발본부 백엔드PT 김설아
- * @since 2021.05.04
+ * @author 솔비포스 WEB개발팀 권지현
+ * @since 2021.11.26
  * @version 1.0
  *
  *  Copyright (C) by SOLBIPOS CORP. All right reserved.
