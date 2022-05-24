c7504fea29d0a2f9e35973a88ffee52b3062f342
sdffd2605 <sdffd2605@solbipos.co.kr>
2022-05-24 17:34:33 +0900

    - 관리자 접속시 솔비포스인 사람만 화면 오픈

--- src/main/java/kr/co/solbipos/adi/sms/smsChargeTab/web/SmsChargeTabController.java
+++ src/main/java/kr/co/solbipos/adi/sms/smsChargeTab/web/SmsChargeTabController.java
@@ -6,6 +6,7 @@ import kr.co.common.data.structure.Result;
 import kr.co.common.service.session.SessionService;
 import kr.co.common.utils.grid.ReturnUtil;
 import kr.co.solbipos.application.session.auth.service.SessionInfoVO;
+import kr.co.solbipos.adi.sms.smsCharge.service.SmsChargeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -41,13 +42,15 @@ import static org.springframework.util.ObjectUtils.isEmpty;
 public class SmsChargeTabController {
 
     private final SessionService sessionService;
+    private final SmsChargeService smsChargeService; // SMS충전
 
     /**
      * Constructor Injection
      */
     @Autowired
-    public SmsChargeTabController(SessionService sessionService) {
+    public SmsChargeTabController(SessionService sessionService, SmsChargeService smsChargeService) {
         this.sessionService = sessionService;
+        this.smsChargeService = smsChargeService; // SMS충전
     }
 
     /**
@@ -60,6 +63,8 @@ public class SmsChargeTabController {
     @RequestMapping(value = "/smsChargeTab/list.sb", method = RequestMethod.GET)
     public String smsChargeTabView(HttpServletRequest request, HttpServletResponse response, Model model) {
 
+        SessionInfoVO sessionInfoVO = sessionService.getSessionInfo(request);
+
         // SMS충전결제 후 충전모듈을 닫기위해 페이지 이동
         String tabVal = "";
         System.out.println("tabVal : " + request.getParameter("tabVal"));
@@ -67,6 +72,11 @@ public class SmsChargeTabController {
             tabVal = request.getParameter("tabVal");
         }
         model.addAttribute("tabVal", tabVal);
+
+        // 전송요금설정 - 화면 권한 체크
+        String msgOneAmtSettingAuth = smsChargeService.msgOneAmtSettingAuth(sessionInfoVO);
+        model.addAttribute("msgOneAmtSettingAuth", msgOneAmtSettingAuth);
+        System.out.println("msgOneAmtSettingAuth : " + msgOneAmtSettingAuth);
 
         return "adi/sms/smsChargeTab/smsChargeTab";
     }
