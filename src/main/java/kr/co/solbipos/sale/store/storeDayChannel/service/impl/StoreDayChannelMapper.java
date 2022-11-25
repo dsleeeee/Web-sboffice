16e56910572e3e2a1f260e43d967a04a783d4d88
jih-k <jhkwon@solbipos.co.kr>
2022-11-25 15:58:09 +0900

    [맘스터치] - [매장분석] - [매장-일별매출현황(채널별)](신규)

---
+++ src/main/java/kr/co/solbipos/sale/store/storeDayChannel/service/impl/StoreDayChannelMapper.java
@@ -0,0 +1,17 @@
+package kr.co.solbipos.sale.store.storeDayChannel.service.impl;
+
+import kr.co.common.data.structure.DefaultMap;
+import kr.co.solbipos.sale.store.storeDayChannel.service.StoreDayChannelVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface StoreDayChannelMapper {
+
+    /** 일별 리스트 조회 */
+    List<DefaultMap<String>> getDayList(StoreDayChannelVO storeDayChannelVO);
+
+}
