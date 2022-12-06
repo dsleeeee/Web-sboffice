1ea701d0981d1e0e0a4de65ede5e430411d6ad7b
sdffd2605 <sdffd2605@solbipos.co.kr>
2022-12-06 11:35:43 +0900

    정산관리>정산관리>매출수기등록 신규, 수정 팝업 - 상단 그리드 상품유형 추가 - 하단 그리드 상품유형 추가 - 하단 그리드 바코드 조회 오류 수정

--- src/main/webapp/resources/solbipos/js/excclc/excclc/saleRegist/newRegist.js
+++ src/main/webapp/resources/solbipos/js/excclc/excclc/saleRegist/newRegist.js
@@ -23,6 +23,8 @@ app.controller('selectProdCtrl', ['$scope', '$http', function ($scope, $http) {
 
     // grid 초기화 : 생성되기전 초기화되면서 생성된다
     $scope.initGrid = function (s, e) {
+        $scope.prodTypeFgDataMap = new wijmo.grid.DataMap(prodTypeFgData, 'value', 'name'); // 상품유형구분
+
         // ReadOnly 효과설정
         s.formatItem.addHandler(function (s, e) {
             if (e.panel === s.cells) {
@@ -101,6 +103,7 @@ app.controller('newRegistCtrl', ['$scope', '$http', function ($scope, $http) {
 
     // grid 초기화 : 생성되기전 초기화되면서 생성된다
     $scope.initGrid = function (s, e) {
+        $scope.prodTypeFgDataMap = new wijmo.grid.DataMap(prodTypeFgData, 'value', 'name'); // 상품유형구분
 
         // 합계
         // add the new GroupRow to the grid's 'columnFooters' panel
