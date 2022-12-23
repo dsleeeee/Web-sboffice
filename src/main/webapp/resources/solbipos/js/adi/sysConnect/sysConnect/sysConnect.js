62b3fee35f1f96b6306efd36bce7d9aee73a4917
jih-k <jhkwon@solbipos.co.kr>
2022-12-23 11:17:49 +0900

    [부가서비스] - [매입정산] - [매입정산시스템접속](신규)

---
+++ src/main/webapp/resources/solbipos/js/adi/sysConnect/sysConnect/sysConnect.js
@@ -0,0 +1,39 @@
+/****************************************************************
+ *
+ * 파일명 : sysConnect.js
+ * 설  명 : 매입정산시스템접속 JavaScript
+ *
+ *    수정일      수정자      Version        Function 명
+ * ------------  ---------   -------------  --------------------
+ * 2022.12.22     권지현      1.0
+ *
+ * **************************************************************/
+/**
+ * get application
+ */
+var app = agrid.getApp();
+
+/**
+ *  예약현황 목록 생성
+ */
+app.controller('sysConnectCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    // 상위 객체 상속 : T/F 는 picker
+    angular.extend(this, new RootController('sysConnectCtrl', $scope, $http, false));
+
+    $scope.init = function (s, e) {
+        $scope._broadcast('sysConnectCtrl');
+    };
+
+    // <-- 검색 호출 -->
+    $scope.$on("sysConnectCtrl", function(event, data) {
+        $scope.sysConnect();
+        event.preventDefault();
+    });
+    
+    // 시스템 접속
+    $scope.sysConnect = function(){
+        window.open(envst1260 + storeCd);
+    };
+
+}]);
