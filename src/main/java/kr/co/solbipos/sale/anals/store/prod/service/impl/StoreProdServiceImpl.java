959df30bbf23f8b299f0d5cebff60500a9bc36bd
dslee <dslee@solbipos.co.kr>
2023-07-17 10:07:28 +0900

    [매출관리] - [매출분석] - [매장별매출분석] - [매장상품순위 탭] 1. 엑셀 다운로드 오류 수정
    Signed-off-by: 이다솜 <dslee@solbipos.co.kr>

--- src/main/java/kr/co/solbipos/sale/anals/store/prod/service/impl/StoreProdServiceImpl.java
+++ src/main/java/kr/co/solbipos/sale/anals/store/prod/service/impl/StoreProdServiceImpl.java
@@ -41,8 +41,10 @@ public class StoreProdServiceImpl implements StoreProdService {
     /** 매장상품순위 - 매장상품순위 리스트(엑셀) 조회   */
     @Override
     public List<DefaultMap<String>> getStoreProdExcelList(StoreProdVO storeProdVO, SessionInfoVO sessionInfoVO) {
-  
+
+        storeProdVO.setOrgnFg(sessionInfoVO.getOrgnFg().getCode());
     	storeProdVO.setHqOfficeCd(sessionInfoVO.getHqOfficeCd());
+        storeProdVO.setEmpNo(sessionInfoVO.getEmpNo());
     	
         if(!StringUtil.getOrBlank(storeProdVO.getStoreCd()).equals("")) {
         	storeProdVO.setArrStoreCd(storeProdVO.getStoreCd().split(","));
