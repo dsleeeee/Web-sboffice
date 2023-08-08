0118eb0977bd30ff2977fd4079d0294157a1415a
sdffd2605 <sdffd2605@solbipos.co.kr>
2023-08-08 14:26:00 +0900

    - 그리드 1개 엑셀다운로드 기능 추가 - 페이징 사용 안하고 있으므로 UI 제거 - 조회기간 63일 제한

--- src/main/webapp/resources/solbipos/js/base/store/storeType/storeTypeChgHist.js
+++ src/main/webapp/resources/solbipos/js/base/store/storeType/storeTypeChgHist.js
@@ -27,8 +27,6 @@ app.controller('storeTypeChgHistCtrl', ['$scope', '$http', '$timeout', function 
     // 상위 객체 상속 : T/F 는 picker
     angular.extend(this, new RootController('storeTypeChgHistCtrl', $scope, $http, true));
 
-    //페이지 스케일 콤보박스 데이터 Set
-    $scope._setComboData("listScaleBox", gvListScaleBoxData);
     $scope._setComboData("stchProcFg", procFgData);
 
     // 변경일자 셋팅
@@ -36,7 +34,6 @@ app.controller('storeTypeChgHistCtrl', ['$scope', '$http', '$timeout', function 
     var endDate = wcombo.genDateVal("#stchEndDate", gvEndDate);
 
     $scope.initGrid = function (s, e) {
-
         // 그리드 DataMap 설정
         $scope.useYnDataMap = new wijmo.grid.DataMap(useYnData, 'value', 'name'); // 사용여부
         $scope.procFgDataMap = new wijmo.grid.DataMap(procFgData, 'value', 'name'); // 입력구분
@@ -96,27 +93,62 @@ app.controller('storeTypeChgHistCtrl', ['$scope', '$http', '$timeout', function 
                 }
             }
         }
-
     };
 
     $scope.$on("storeTypeChgHistCtrl", function(event, data) {
         // 매장타입변경이력 조회
         $scope.srchStoreTypeChgHist();
         event.preventDefault();
-
     });
 
     // 매장타입변경이력 조회
     $scope.srchStoreTypeChgHist =  function () {
+        var startDt = new Date(wijmo.Globalize.format(startDate.value, 'yyyy-MM-dd'));
+        var endDt = new Date(wijmo.Globalize.format(endDate.value, 'yyyy-MM-dd'));
+        var diffDay = (endDt.getTime() - startDt.getTime()) / (24 * 60 * 60 * 1000); // 시 * 분 * 초 * 밀리세컨
+
+        // 시작일자가 종료일자보다 빠른지 확인
+        if(startDt.getTime() > endDt.getTime()){
+            $scope._popMsg(messages['cmm.dateChk.error']);
+            return false;
+        }
+        // 조회일자 최대 3달(93일) 제한
+        if (diffDay > 93) {
+            $scope._popMsg(messages['cmm.dateOver.3month.error']);
+            return false;
+        }
+
         var params = {};
         params.startDate = wijmo.Globalize.format(startDate.value, 'yyyyMMdd'); // 조회기간
         params.endDate = wijmo.Globalize.format(endDate.value, 'yyyyMMdd'); // 조회기간
         params.storeTypeCd = $("#stchStoreTypeCd").val();
         params.storeTypeNm = $("#stchStoreTypeNm").val();
         params.procFg = $scope.stchProcFgCombo.selectedValue;
-        //params.listScale = $scope.listScale;
 
         $scope._inquiryMain('/base/store/storeType/storeType/getStoreTypeChgHist.sb', params);
-    }
+    };
+
+    // 엑셀다운로드
+    $scope.excelDownload = function () {
+        if ($scope.flex.rows.length <= 0) {
+            $scope._popMsg(messages["excelUpload.not.downloadData"]); // 다운로드 할 데이터가 없습니다.
+            return false;
+        }
+
+        $scope.$broadcast('loadingPopupActive', messages["cmm.progress"]); // 데이터 처리중 메시지 팝업 오픈
+        $timeout(function () {
+            wijmo.grid.xlsx.FlexGridXlsxConverter.saveAsync($scope.flex, {
+                includeColumnHeaders: true,
+                includeCellStyles: true,
+                includeColumns: function (column) {
+                    return column.visible;
+                }
+            }, '매장타입변경이력_' + getToday() + '.xlsx', function () {
+                $timeout(function () {
+                    $scope.$broadcast('loadingPopupInactive'); // 데이터 처리중 메시지 팝업 닫기
+                }, 10);
+            });
+        }, 10);
+    };
 
 }]);
