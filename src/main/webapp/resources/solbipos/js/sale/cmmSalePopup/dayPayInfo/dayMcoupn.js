39d416f3002076966902ca56dee5fe6b960ac909
sdffd2605 <sdffd2605@solbipos.co.kr>
2023-10-31 14:46:43 +0900

    매출분석>매출현황>월별>월별종합 탭>모바일쿠폰 팝업 - 모바일쿠폰 로직 수정   : TB_CM_NMCODE.NMCODE_GRP_CD='166' -> TB_CM_VAN_CMPNY.VAN_FG = '04'

--- src/main/webapp/resources/solbipos/js/sale/cmmSalePopup/dayPayInfo/dayMcoupn.js
+++ src/main/webapp/resources/solbipos/js/sale/cmmSalePopup/dayPayInfo/dayMcoupn.js
@@ -21,8 +21,6 @@ app.controller('dayMcoupnCtrl', ['$scope', '$http', '$timeout', function ($scope
 
   // grid 초기화 : 생성되기전 초기화되면서 생성된다
   $scope.initGrid = function (s, e) {
-    $scope.mcoupnCdMap = new wijmo.grid.DataMap(mcouponCd, 'value', 'name'); // 쿠폰종류
-
     // 그리드 링크 효과
     s.formatItem.addHandler(function (s, e) {
       if (e.panel === s.cells) {
