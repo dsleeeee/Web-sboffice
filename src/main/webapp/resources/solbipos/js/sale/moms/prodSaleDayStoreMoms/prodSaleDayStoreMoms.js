505bfb7c0307f542c2a0abea4f956840dfd81a2b
sdffd2605 <sdffd2605@solbipos.co.kr>
2024-01-25 11:19:20 +0900

    간소화화면 기능사용제한 - 분할 엑셀다운로드 사용자 제한 (사용자 아이디로 처리)   : [168 간소화다운로드기능사용자]

--- src/main/webapp/resources/solbipos/js/sale/moms/prodSaleDayStoreMoms/prodSaleDayStoreMoms.js
+++ src/main/webapp/resources/solbipos/js/sale/moms/prodSaleDayStoreMoms/prodSaleDayStoreMoms.js
@@ -207,7 +207,25 @@ app.controller('prodSaleDayStoreMomsCtrl', ['$scope', '$http', '$timeout', funct
         params.momsStoreFg01 = $scope.momsStoreFg01;
         params.excelType = excelType;
 
-        $scope._broadcast('prodSaleDayStoreMomsExcelCtrl', params);
+        if(params.excelType === '1') {
+            // 데이터양에 따라 2-3초에서 수분이 걸릴 수도 있습니다.
+            $scope._popConfirm(messages["cmm.excel.totalExceDownload"], function() {
+                $scope._broadcast('prodSaleDayStoreMomsExcelCtrl', params);
+            });
+        }else{
+            // 분할 엑셀다운로드 사용자 제한
+            $scope._postJSONQuery.withOutPopUp('/sale/moms/prodSaleDayStoreMoms/prodSaleDayStoreMoms/getDivisionExcelDownloadUserIdChk.sb', params, function (response) {
+                if (response.data.data.list === 0) {
+                    $scope._popMsg(messages["prodSaleDayStoreMoms.userIdChkAlert"]); // 사용권한이 없습니다.
+                    return;
+                } else {
+                    // 데이터양에 따라 2-3초에서 수분이 걸릴 수도 있습니다.
+                    $scope._popConfirm(messages["cmm.excel.totalExceDownload"], function() {
+                        $scope._broadcast('prodSaleDayStoreMomsExcelCtrl', params);
+                    });
+                }
+            });
+        }
     };
 
     // 현재화면 엑셀다운로드
@@ -272,24 +290,9 @@ app.controller('prodSaleDayStoreMomsExcelCtrl', ['$scope', '$http', '$timeout', 
     // <-- 검색 호출 -->
     $scope.$on("prodSaleDayStoreMomsExcelCtrl", function (event, data) {
         if(data.excelType === '1') {
-            // 데이터양에 따라 2-3초에서 수분이 걸릴 수도 있습니다.
-            $scope._popConfirm(messages["cmm.excel.totalExceDownload"], function() {
-                $scope.searchExcelList(data);
-            });
+            $scope.searchExcelList(data);
         }else{
-            // 분할 엑셀다운로드 사용자 제한
-            var params = {};
-            $scope._postJSONQuery.withOutPopUp('/sale/moms/prodSaleDayStoreMoms/prodSaleDayStoreMoms/getDivisionExcelDownloadUserIdChk.sb', params, function (response) {
-                if (response.data.data.list === 0) {
-                    $scope._popMsg(messages["prodSaleDayStoreMoms.userIdChkAlert"]); // 사용권한이 없습니다.
-                    return;
-                } else {
-                    // 데이터양에 따라 2-3초에서 수분이 걸릴 수도 있습니다.
-                    $scope._popConfirm(messages["cmm.excel.totalExceDownload"], function() {
-                        $scope.searchExcelDivisionList(data);
-                    });
-                }
-            });
+            $scope.searchExcelDivisionList(data);
         }
         event.preventDefault();
     });
