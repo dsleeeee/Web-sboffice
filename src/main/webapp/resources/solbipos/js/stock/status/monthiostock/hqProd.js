91a96dc671391b1a29f0c186a79d47ea6becc74e
sdffd2605 <sdffd2605@solbipos.co.kr>
2024-06-05 17:44:07 +0900

    [재고관리] - [재고현황] - [월수불현황] - 그리드 상품코드 클릭시 오류 수정 - 그리드 기초재고, 기말재고 클릭 제거 (팝업 없음) - 그리드 세트생성, 거래처출고, 거래처반품 히든

--- src/main/webapp/resources/solbipos/js/stock/status/monthiostock/hqProd.js
+++ src/main/webapp/resources/solbipos/js/stock/status/monthiostock/hqProd.js
@@ -591,10 +591,12 @@ app.controller('monthIostockExcelCtrl', ['$scope', '$http', '$timeout', function
 		if(check == '1'){ // 수량
 			for(var i=6; i<length; i++){
 				var colLength = columns[i].binding.length;
-				if(columns[i].binding.slice(-3) == 'Tot'){
-					columns[i].visible = false;
-				}else if(columns[i].binding.slice(-3) == 'Qty' && columns[i].binding != 'setInQty' && columns[i].binding != 'saleVendrOrderQty' && columns[i].binding != 'saleVendrOrderTot' && columns[i].binding != 'saleVendrRtnQty' && columns[i].binding != 'saleVendrRtnTot'){
-					columns[i].visible = true;
+				if(columns[i].binding != 'setInQty' && columns[i].binding != 'saleVendrOrderQty' && columns[i].binding != 'saleVendrOrderTot' && columns[i].binding != 'saleVendrRtnQty' && columns[i].binding != 'saleVendrRtnTot') {
+					if (columns[i].binding.slice(-3) == 'Tot') {
+						columns[i].visible = false;
+					} else if (columns[i].binding.slice(-3) == 'Qty') {
+						columns[i].visible = true;
+					}
 				}
 			}
 		}else if(check == '2'){ // 금액
