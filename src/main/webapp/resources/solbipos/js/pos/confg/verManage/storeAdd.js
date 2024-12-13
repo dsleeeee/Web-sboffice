9061342023b5f6d770d7089e9ab72d97756ba61d
dslee <dslee@solbipos.co.kr>
2024-12-13 13:24:54 +0900

    [포스관리] - [POS 설정관리] - [NXPOS 버전관리] - [적용매장 탭] - DS89117은 예외 처리(신규버전등록 버튼 클릭시, 이미 다른 버전에 적용되었는지 체크 하는 부분)(신규버전예약등록 예외처리 부분 원복)
    Signed-off-by: 이다솜 <dslee@solbipos.co.kr>

--- src/main/webapp/resources/solbipos/js/pos/confg/verManage/storeAdd.js
+++ src/main/webapp/resources/solbipos/js/pos/confg/verManage/storeAdd.js
@@ -213,12 +213,10 @@ app.controller('addStoreCtrl', ['$scope', '$http', '$timeout', function ($scope,
     // NXPOS_V1/V2 외에 적용매장등록된 내역이 있습니다.<br>[포스관리] - [POS 설정관리] - [매장별 POS 버전 삭제] 에서 관리 후 진행하여 주십시오.
     for (var i = 0; i < sScope.flex.collectionView.items.length; i++) {
       if (sScope.flex.collectionView.items[i].gChk) {
-          if(sScope.flex.collectionView.items[i].storeCd !== "DS89117") { // DS89117은 예외 처리
-              if (sScope.flex.collectionView.items[i].diffVerCnt > 0) {
-                  sScope._popMsg("NXPOS_V" + manageVer + messages["verManage.store.diffVerCnt.chk.msg"]);
-                  return false;
-              }
-          }
+        if(sScope.flex.collectionView.items[i].diffVerCnt > 0){
+          sScope._popMsg( "NXPOS_V" + manageVer + messages["verManage.store.diffVerCnt.chk.msg"]);
+          return false;
+        }
       }
     }
 
