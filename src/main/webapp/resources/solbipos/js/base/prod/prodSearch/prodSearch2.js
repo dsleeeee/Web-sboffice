2234cd37c1d324b7faf85d4e719c8d9ddb3c7fcf
dslee <dslee@solbipos.co.kr>
2025-02-03 19:32:37 +0900

    [기초관리] - [상품관리2] - [상품정보조회(추가정보)] - 등록상품 or 등록매장 삭제 시, 상품정보 테이블 변경이력 (MOD_DT, MOD_ID) 추가

--- src/main/webapp/resources/solbipos/js/base/prod/prodSearch/prodSearch2.js
+++ src/main/webapp/resources/solbipos/js/base/prod/prodSearch/prodSearch2.js
@@ -478,6 +478,10 @@ app.controller('prodctrl2', ['$scope', '$http', '$timeout', function ($scope, $h
 
     // 조회조건내역 엑셀 다운로드
     $scope.excelDownloadCondition = function () {
+
+        $scope._popMsg("수정 중 입니다.");
+        return false;
+
         // 파라미터
         var params = {};
         // 등록일자 '전체기간' 선택에 따른 params
@@ -536,6 +540,10 @@ app.controller('prodctrl2', ['$scope', '$http', '$timeout', function ($scope, $h
 
     // 전체 엑셀 다운로드
     $scope.excelDownloadTotal = function () {
+
+        $scope._popMsg("수정 중 입니다.");
+        return false;
+
         // 파라미터
         var params = {};
         // 등록일자 '전체기간' 선택에 따른 params
