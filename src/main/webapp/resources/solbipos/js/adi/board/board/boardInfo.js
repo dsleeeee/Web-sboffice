416e293c024dad679fd6ea3b17afbab85e79b898
sdffd2605 <sdffd2605@solbipos.co.kr>
2025-05-07 16:03:19 +0900

    [부가서비스] - [게시판] - [공지사항] - 체크로직 리턴시 로딩바 제거

--- src/main/webapp/resources/solbipos/js/adi/board/board/boardInfo.js
+++ src/main/webapp/resources/solbipos/js/adi/board/board/boardInfo.js
@@ -582,6 +582,7 @@ app.controller('boardInfoCtrl', ['$scope', '$http', '$timeout', function ($scope
         || params.content.toLowerCase().indexOf('onunload') >= 0
         )
         {
+            $scope.$broadcast('loadingPopupInactive'); //데이터 처리중 메시지 팝업 닫기
             $scope._popMsg(messages["boardInfo.scriptChk.msg"]);
             return false;
         }
