2e6611d6d0ca16db494965b39342dc48159dfbf1
dslee <dslee@solbipos.co.kr>
2026-01-09 14:11:09 +0900

    1. [OMS] [백오피스] 오더앤라이더 배달대행사 등록 API 연동 요청의 건 	1. [오더킷] - [오더킷] - [배달대행사 연동] - [배달대행사 추가] 팝업 		: 배달대행사, 하위 배달대행사 콤보박스 값 정렬 수정(직접배달/부릉제외, 이름 오름차순 정렬) 	2. [오더킷] - [오더킷] - [배달대행사 연동] 		: 유저상태조회 API 호출 후 응답값 "data" : null 일때 화면 로직 추가 	3. 유저상태조회 API 호출 응답값에 따른 오더킷 웹 사이트 URL 분기처리 		: [기초관리] - [상품관리] - [상품정보관리] 		: [기초관리] - [상품관리] - [사이드메뉴관리] 		: [오더킷] - [오더킷] - [오더킷] 		: [오더킷] - [오더킷] - [원산지관리] 		: [오더킷] - [오더킷] - [배달대행사 연동] 	4. 연동 URL 변경 (테스트 URL) 		: 오더킷 웹 사이트 URL 		: 유저상태조회 API URL, 주문연동활성화변경 API URL

--- src/main/webapp/resources/solbipos/js/base/prod/prod/prod.js
+++ src/main/webapp/resources/solbipos/js/base/prod/prod/prod.js
@@ -596,18 +596,53 @@ app.controller('prodCtrl', ['$scope', '$http', '$timeout', function ($scope, $ht
   // 오더킷 바로가기
   $scope.orderkitGoto = function () {
 
-      var params = {};
-      $scope._postJSONQuery.withOutPopUp('/orderkit/orderkit/orderkitRecpOrigin/orderkitGoto.sb', params, function (response) {
+    var params = {};
+    var url = "https://test.orderkit.co.kr/"; // 개발
+    //var url = "https://orderkit.co.kr/"; // 운영
 
-          // jwtToken
-          var jwtToken = response.data.data;
+    $scope._postJSONQuery.withOutPopUp('/orderkit/orderkit/orderkitRecpOrigin/orderkitGoto.sb', params, function (response) {
 
-          // OMS웹뷰 새창 open
-          var url = "https://kcp.onesell.co.kr/auth/pos?token=" + jwtToken; // 개발
-          //var url = "https://orderkit.co.kr/auth/pos?token=" + jwtToken; // 운영
-          window.open(url, 'newWindow');
+      // jwtToken
+      var jwtToken = response.data.data;
 
-      });
+      if (response.data.status === 'OK') {
+        if (jwtToken.length > 0) {
+
+          // 유저 상태 조회(구독여부, 주문 중개 서비스 사용여부, 배달앱 연동 정보)
+          $scope._postJSONQuery.withOutPopUp("/dlvr/manage/info/dlvrAgencyLink/getOmsUserStatus.sb", params, function (response) {
+
+            var data = response.data.data.list;
+
+            if (data.status === "success" && data.status_code === 200) {
+
+              if (data.data.paymentStatus == "SUSPENDED" || data.data.paymentStatus == "GRACE" || data.data.paymentStatus == "WITHDRAW") { // 사용중지, 유예, 탈퇴
+                console.log("url : " + url + "auth/pos?token=" + jwtToken);
+                window.open(url + "auth/pos?token=" + jwtToken, 'newWindow');
+              } else if (data.data.paymentStatus == "UNPAID") { // 미결제
+                console.log("url : " + url + "app/payment/pay?token=" + jwtToken);
+                window.open(url + "app/payment/pay?token=" + jwtToken, 'newWindow');
+              } else if (data.data.paymentStatus == "ACTIVE") { // 결제 완료
+                if (data.data.base_platform_info === null) { // 배달앱 미연동
+                  console.log("url : " + url + "app/menu/collect?token=" + jwtToken);
+                  window.open(url + "app/menu/collect?token=" + jwtToken, 'newWindow');
+                }
+                if (data.data.base_platform_info !== null) { // 정상 연동
+                  console.log("url : " + url + "app/dashboard?token=" + jwtToken);
+                  window.open(url + "app/dashboard?token=" + jwtToken, 'newWindow');
+                }
+              } else {
+                console.log("url : " + url + "auth/pos?token=" + jwtToken);
+                window.open(url + "auth/pos?token=" + jwtToken, 'newWindow');
+              }
+
+            } else { // data.status === "error" && data.status_code === 500 인 상태, 비회원
+              console.log("url : " + url + "auth/pos?token=" + jwtToken);
+              window.open(url + "auth/pos?token=" + jwtToken, 'newWindow');
+            }
+          });
+        }
+      }
+    });
   }
 
 
